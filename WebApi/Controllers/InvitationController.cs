0b6a634d5679d893a6b50ea86c5aed59c90a9c20
Rivka <rbn9574@gmail.com>
2024-06-26 12:18:51 +0300

    trips project

---
+++ WebApi/Controllers/InvitationController.cs
@@ -0,0 +1,49 @@
+﻿using BLL.Interfaces;
+using DTO.Classes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvitationController : ControllerBase
+    {
+        IInvitationBll bll;
+
+        public InvitationController(IInvitationBll bll)
+        {
+            this.bll = bll;
+        }
+
+        [HttpGet]
+        public List<InvitationDTO> GetAll()
+        {
+            return  this.bll.GetAll();
+        }
+
+        [HttpGet("GetById/{InvitationId}")]
+        public async Task<InvitationDTO> GetById(int InvitationId)
+        {
+            return await this.bll.GetById(InvitationId);
+        }
+
+        [HttpPost]
+        public async Task<int> Add(InvitationDTO invitation)
+        {
+            return await this.bll.Add(invitation);
+        }
+
+        [HttpDelete("{InvitationId}")]
+        public async Task<bool> Delete(int InvitationId)
+        {
+            return await this.bll.Delete(InvitationId);
+        }
+
+        [HttpGet("GetAllInvitationsToTrip/{tripId}")]
+        public async Task<List<InvitationDTO>> GetAllInvitationsToTrip(int tripId)
+        {
+            return await bll.GetAllInvitationsToTrip(tripId);
+        }
+    }
+}
